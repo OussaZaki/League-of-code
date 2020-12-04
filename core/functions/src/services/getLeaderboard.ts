9f07961f854b2dc60bed43215db0948129445383
Svante Gustafsson <svante.gustafsson@klarna.com>
2020-12-04 14:03:22 +0100

    updated to 2020 leaderboard

--- core/functions/src/services/getLeaderboard.ts
+++ core/functions/src/services/getLeaderboard.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import { AdventLeaderboard } from '../models/AdventLeaderboard';
 
 const LEADERBOADR_URL =
-  'https://adventofcode.com/2020/leaderboard/private/view/12900.json';
+  'https://adventofcode.com/2020/leaderboard/private/view/33370.json';
 const SESSION_COOKIE =
   'session=53616c7465645f5f06fe8773781fc09807b1711158f13d23df729a97a5225f5acf7245157ac69909afc7d76969e1f519';
 
